76c0fd48c47eafc222dd357826280eb0898921fc
Alex Lu <alu59@my.bcit.ca>
2025-10-14 18:38:35 -0700

    Add atrribute "active" to nav-item for text colour

--- src/components/nav-item.js
+++ src/components/nav-item.js
@@ -1,15 +1,37 @@
 class NavItem extends HTMLElement {
+
+    static get observedAttributes() {
+        return [`active`]
+    }
+
     constructor() {
         super()
+        this.active = false
         this.content = this.innerHTML
         this.render()
     }
     render() {
         this.innerHTML = `
-        <div class = "text-gray-900 hover:text-gray-500 hover:bg-gray-800/60 rounded-full p-4 w-fit h-[100%] transition-all duration-250 max-h-fit">
+        <div class = "${this.active ? 'text-white' : 'text-gray-600'} hover:bg-gray-800/60 rounded-full p-4 w-fit h-[100%] transition-all duration-250 max-h-fit">
         ${this.content}
         </div>
         `;
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "active") {
+            // switch (newValue) {
+            //     case "true":
+            //         this.active = true
+            //     case "false":
+            //         this.active = false
+            //     default:
+            //         console.log("active should be true or false but isn't")
+            // }
+            this.active = newValue
+            console.log(newValue)
+            this.render()
+        }
     }
 }
 
