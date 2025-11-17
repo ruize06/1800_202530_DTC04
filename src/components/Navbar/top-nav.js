5d2696ec82d274700a45cbbe5dfc5be241172537
Meiqi Zhao <meiqizhao002@gmail.com>
2025-11-16 21:50:13 -0800

    Fix top-nav.js

--- src/components/Navbar/top-nav.js
+++ src/components/Navbar/top-nav.js
@@ -9,20 +9,19 @@ class TopNav extends HTMLElement {
     this.render();
   }
 
-  render() {
+  render(title = this.title) {
     this.innerHTML = `
       <nav class="fixed top-0 left-0 z-50 w-full bg-white border-b border-gray-200">
         <div class="max-w-5xl mx-auto py-4 px-6 flex items-center justify-center">
-          <h1 class="text-2xl font-semibold">${this.title}</h1>
+          <h1 class="text-2xl font-semibold">${title}</h1>
         </div>
       </nav>
     `;
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (name === "title") {
-      this.title = newValue;
-      this.render();
+    if (name === "title" && oldValue !== newValue) {
+      this.render(newValue);
     }
   }
 }
