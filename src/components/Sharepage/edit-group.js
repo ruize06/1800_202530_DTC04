7d8cf442a0f99fbf6c6feb3b784e412d71273f88
Ruize Sun <jacksunruize@gmail.com>
2025-11-18 12:59:41 -0800

    add reusable confirmation popup that appears for removing members and leaving groups

--- src/components/Sharepage/edit-group.js
+++ src/components/Sharepage/edit-group.js
@@ -1,7 +1,8 @@
 import { collection, getDoc, getDocs, doc, deleteDoc, updateDoc, arrayRemove, query, where } from "firebase/firestore";
 import { db, auth } from "../../firebaseConfig.js";
 import { onAuthStateChanged } from "firebase/auth";
-import { showPopup, hidePopup } from "/src/utils.js";
+import { showPopup, hidePopup, confirmationPopup } from "/src/utils.js";
+
 
 async function deleteGroup(groupRef) {
     const tasks_q = query(
@@ -49,7 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
         showPopup(popup)
 
         membersList.innerHTML = "";
-        
+
         const groupRef = doc(db, "groups", currentGroupId);
         const groupSnapshot = await getDoc(groupRef);
         const groupData = groupSnapshot.data()
@@ -57,16 +58,16 @@ document.addEventListener("DOMContentLoaded", () => {
         groupNameInput.value = groupData.name
 
         const groupMembers = groupData.members;
-        
+
         groupMembers.forEach(async (memberId) => {
             const memberRef = doc(db, "userprofiles", memberId);
             const memberSnapshot = await getDoc(memberRef);
             const member = memberSnapshot.data();
-            
+
             const memberElement = document.createElement("li");
-            memberElement.id  = memberId
+            memberElement.id = memberId
             memberElement.className =
-            "flex items-center justify-between gap-2 p-2 border rounded";
+                "flex items-center justify-between gap-2 p-2 border rounded";
             membersList.appendChild(memberElement);
 
             // User
@@ -82,8 +83,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 btn.textContent = "Remove";
                 btn.className =
                     "px-3 py-2 text-sm rounded-full text-white bg-[var(--danger-button-bg-color)] hover:bg-[var(--secondary-button-bg-color)] hover:scale-105";
-                btn.addEventListener("click", async () => {
-                    await removeMember(groupRef, memberId)
+                btn.addEventListener("click", () => {
+                    confirmationPopup({
+                        message: `Remove ${member.username} from the group?`,
+                        onConfirm: async () => {
+                            await removeMember(groupRef, memberId);
+                        },
+                    });
                 });
             }
         });
@@ -105,9 +111,15 @@ document.addEventListener("DOMContentLoaded", () => {
         hidePopup(popup)
     });
 
-    leaveBtn.addEventListener("click", async () => {
-        await removeMember(doc(db, "groups", currentGroupId), currentUserId);
-        window.location.href = "/sharepage_Groups.html"
-        alert("Left the group")
+    leaveBtn.addEventListener("click", () => {
+        const groupRef = doc(db, "groups", currentGroupId);
+
+        confirmationPopup({
+            message: "Leave this group?",
+            onConfirm: async () => {
+                await removeMember(groupRef, currentUserId);
+                window.location.href = "/sharepage_Groups.html";
+            },
+        });
     });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,22 @@
 export function isLightColor(hexColor) {
-    if (hexColor.length == 7) {
-      const rgb = [
-        parseInt(hexColor.substring(1, 3), 16),
-        parseInt(hexColor.substring(3, 5), 16),
-        parseInt(hexColor.substring(5), 16),
-      ];
-      const luminance =
-        (0.2126 * rgb[0]) / 255 +
-        (0.7152 * rgb[1]) / 255 +
-        (0.0722 * rgb[2]) / 255;
-      return luminance > 0.5;
-    }
-    return false
+  if (hexColor.length == 7) {
+    const rgb = [
+      parseInt(hexColor.substring(1, 3), 16),
+      parseInt(hexColor.substring(3, 5), 16),
+      parseInt(hexColor.substring(5), 16),
+    ];
+    const luminance =
+      (0.2126 * rgb[0]) / 255 +
+      (0.7152 * rgb[1]) / 255 +
+      (0.0722 * rgb[2]) / 255;
+    return luminance > 0.5;
+  }
+  return false
 }
 
 export function arrayRemove(array, item) {
-    const index = array.indexOf(item);
-    if (index !== -1) array.splice(index, 1);
+  const index = array.indexOf(item);
+  if (index !== -1) array.splice(index, 1);
 }
 
 export function customClone(originalNode, attachNode) {
@@ -36,17 +36,17 @@ export function addEventListeners(target, events, callback) {
 }
 
 export function hidePopup(popupElement, transitionSpeed = 300) {
-    popupElement?.classList.toggle("translate-y-full", true);
-    setTimeout(() => {
-        popupElement?.classList.toggle("hidden", true);
-    }, transitionSpeed);
+  popupElement?.classList.toggle("translate-y-full", true);
+  setTimeout(() => {
+    popupElement?.classList.toggle("hidden", true);
+  }, transitionSpeed);
 }
 
 export function showPopup(popupElement) {
-    popupElement?.classList.toggle("hidden", false);
-    setTimeout(() => {
-        popupElement?.classList.toggle("translate-y-full", false);
-    }, 0);
+  popupElement?.classList.toggle("hidden", false);
+  setTimeout(() => {
+    popupElement?.classList.toggle("translate-y-full", false);
+  }, 0);
 }
 
 export function addPopupEventListeners(openButton, closeButton, popupElement, onOpen, onClose) {
@@ -60,20 +60,61 @@ export function addPopupEventListeners(openButton, closeButton, popupElement, on
    * @param onClose (callableFunction) - callback function called when the menu is closed
    */
   // Open popup
-  const openFunc = (e) => {onOpen(e);/* showPopup(popupElement)*/}
-  const closeFunc = (e) => {onClose(e);/* hidePopup(popupElement)*/}
+  const openFunc = (e) => { onOpen(e);/* showPopup(popupElement)*/ }
+  const closeFunc = (e) => { onClose(e);/* hidePopup(popupElement)*/ }
   openButton?.addEventListener("click", openFunc);
   // Cancel popup
   closeButton?.addEventListener("click", closeFunc);
   if (popupElement?.getAttribute("listener") !== "true") {
     popupElement?.setAttribute("listener", "true")
     popupElement?.addEventListener(
-        "focusout", () => {
-            if (document.hasFocus()) popupElement.timer = setTimeout(closeFunc, 0);
-    });
+      "focusout", () => {
+        if (document.hasFocus()) popupElement.timer = setTimeout(closeFunc, 0);
+      });
     popupElement?.addEventListener(
-        "focusin", () => {
-            clearTimeout(popupElement.timer);
-    });
+      "focusin", () => {
+        clearTimeout(popupElement.timer);
+      });
   }
+}
+
+export function confirmationPopup({ message, onConfirm }) {
+  const popup = document.createElement("div");
+  popup.id = "confirmationPopup";
+  popup.className = `fixed bottom-1/2 left-1/2 -translate-x-1/2 w-full md:max-w-[50%] max-w-[90%]
+    bg-[var(--secondary-bg-color)] text-[var(--text-color)] rounded-xl px-8 pb-8 shadow-lg
+    translate-y-full transition-transform duration-300 z-30 ease-in-out hidden`;
+
+  popup.innerHTML = `
+    <!-- Header -->
+    <div class="sticky flex flex-row items-center justify-between top-0 bg-inherit py-4 border-b border-[var(--primary-border-color)]">
+      <p class="text-xl font-bold">${message}</p>
+    </div>
+    <div class="flex max-w-[90%] mx-auto justify-between pt-5">
+        <button id="confirmBtn" type="button"
+                class="px-6 py-2 rounded-full bg-[var(--danger-button-bg-color)] text-white 
+                       hover:bg-[var(--secondary-button-bg-color)] hover:scale-105">
+          Confirm
+        </button>
+        <button id="cancelBtn"
+                class="px-6 py-2 rounded-full bg-gray-500 text-white 
+                       hover:bg-gray-600 hover:scale-105">
+          Cancel
+        </button>
+      </div>
+  `;
+
+  document.body.appendChild(popup);
+
+  popup.querySelector("#confirmBtn").addEventListener("click", () => {
+    onConfirm?.();
+    hidePopup(popup);
+  });
+
+  popup.querySelector("#cancelBtn").addEventListener("click", () => {
+    hidePopup(popup);
+  });
+
+  showPopup(popup);
+  return popup;
 }
